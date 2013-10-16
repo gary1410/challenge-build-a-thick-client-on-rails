60af68fba4b150b3a37b441398b22fd6e7d835c5
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-10-16 10:29:35 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Quizzes::Application.routes.draw do
   root to: 'home#index'
+
+  get '/quizzes.:format', to: 'quizzes#index', as: :quizzes, constraints: {:format => /json/}
+  get '/quizzes/:quiz_id/questions/next.:format', to: 'questions#next', as: :next_question, constraints: {:format => /json/}
+  post '/questions/:question_id/answers.:format', to: 'questions#answer', as: :submit_answer, constraints: {:format => /json/}
 end
