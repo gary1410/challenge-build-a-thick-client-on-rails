5ac35967948275a0c8910e396991d96a0265f05f
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-10-16 19:30:07 -0700

    handle nil case

--- app/models/simple_session.rb
+++ app/models/simple_session.rb
@@ -2,6 +2,8 @@ class SimpleSession < ActiveRecord::Base
   attr_accessible :session_key, :last_answered_question_id
 
   def tally(submitted_choice, correct_choice)
+    self.num_correct = 0 if self.num_correct.nil?
+    self.num_incorrect = 0 if self.num_incorrect.nil?
     if submitted_choice.id == correct_choice.id
       self.num_correct += 1
     else
