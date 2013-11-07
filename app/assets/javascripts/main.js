cb20a043da7109a0c9204d47299def36dccf9521
Apprentice <apprentice@dbc-22.local>
2013-11-06 20:26:41 -0800

    user can click on answers and go to next page, user canview a results page.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -13,11 +13,14 @@
 //    - record if it was correct/incorrect
 //    - ajax to server to get the next question
 //    - display question and choices to the user
+var key = "Holla";
 
 var Controller = {
   getAndDisplayQuizzes: function () {
-    var req = $.ajax({url: '/quizzes.json', type: 'GET'});
-    req.done(function(data){
+    var req = $.ajax({
+      url: '/quizzes.json',
+      type: 'GET'});
+      req.done(function(data){
       // create a link for each quiz, and append it to our starting page
       for( var i = 0; i < data.quizzes.length; i++ ){
         var quizViewer = new QuizViewer(data.quizzes[i])
@@ -27,17 +30,20 @@ var Controller = {
   },
 
   getAndDisplayQuestionsAndAnswers: function(quiz_id) {
-    console.log("You clicked on quiz: "+quiz_id)
     $(event.target).closest('.start').addClass('hidden')
-    var req = $.ajax({url: "/quizzes/"+quiz_id+"/questions/next.json", type: 'GET', data: {session_key: 'a124f87dec55da23'} });
+    var quiz_id = quiz_id
+    var req = $.ajax({
+      url: "/quizzes/"+quiz_id+"/questions/next.json",
+      type: 'GET', data: {session_key: key}
+    });
     // got the next question, now display it
     req.done(function(data){
       var questionView = new QuestionViewer(data.question)
       $('.container').append(questionView.renderQuestion());
+    }).fail(function(data){
+      $('.results').show();
     })
   },
-
-
 
   listenForClickOnQuiz: function () {
    $(".start").on("click", ".quiz-name", function(e){
@@ -52,15 +58,24 @@ var Controller = {
     e.preventDefault();
     var question_id = $(event.target).closest('.question')[0].id
     var answer_id = $(event.target).closest('li')[0].id
+    var quiz_id = $(event.target).closest('.question').data('quiz_id')
     Controller.sendAnswer(question_id, answer_id);
+    $(event.target).closest('.question').remove();
+    Controller.getAndDisplayQuestionsAndAnswers(quiz_id)
   })
  },
 
  sendAnswer: function(question_id, answer_id){
-  $.ajax({url: "/questions/"+question_id+"/answers.json", type: 'POST', data:{session_key: 'a124f87dec55da23', choice_id: answer_id } }).done(function(e){
-    console.log("SENT YOUR ANSWER", e);
-    debugger
-
+  $.ajax({
+    url: "/questions/"+question_id+"/answers.json",
+    type: 'POST', data:{session_key: key, choice_id: answer_id } }).done(function(data){
+    var resp = "incorrect!"
+    if (data.status.correct) {
+      resp = "correct!";
+    }
+    alert("Your answer was "+resp);
+    $(".results").find('#correct').html(data.status.num_correct)
+    $(".results").find('#incorrect').html(data.status.num_incorrect)
   })
  }
 }
@@ -74,6 +89,7 @@ QuestionViewer.prototype.renderQuestion = function() {
   // puts question into h1 and give it the question id
   $questionElem.find('h1').html(this.question.question)
   $questionElem.attr("id", this.question.question_id)
+  $questionElem.data("quiz_id", this.question.choices[0].quiz_id)
   // put all the answers into the list in our template
   var answers = this.question.choices
   for (var i=0;i<answers.length;i++){ $questionElem.find('.answers ul').append(
@@ -105,5 +121,10 @@ $(document).ready(function() {
   Controller.listenForClickOnQuiz();
 
   Controller.listenForClickOnAnswer();
+  // to restart
+
+  // add event listener to restart link
+  // reset a new key, remove everything from dom except for templates and start div
+  //hide div with results
 
 })
